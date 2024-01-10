17237f6e4cae87b5f5bcdbd782282987b926c798
Mateuz332 <mateusbdev332@gmail.com>
2024-01-10 15:09:56 -0300

    surpresa! initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function btntoglle() {
+    const html = document.documentElement
+    html.classList.toggle('light')
+
+    const img = document.querySelector('#startimage')
+
+
+    if (html.classList.contains('light')) {
+        img.setAttribute('src', './assets/myft/Avatar.png')
+    }else {
+        img.setAttribute('src', './assets/myft/Avatar-light.png')
+    }
+}
