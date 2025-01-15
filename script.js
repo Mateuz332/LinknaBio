26ed98e4829813ada6ef67ca58ea1e4879d83b2d
Mateuz332 <mateusbdev332@gmail.com>
2025-01-15 19:45:46 -0300

    modificações para teste

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ function btntoglle() {
     const html = document.documentElement
     html.classList.toggle('light')
 
-    const img = document.querySelector('#startimage')
-
-
-    if (html.classList.contains('light')) {
-        img.setAttribute('src', './assets/myft/Avatar.png')
-    }else {
-        img.setAttribute('src', './assets/myft/Avatar-light.png')
-    }
 }
+
+
+// const img = document.querySelector('#startimage')
+
+
+// if (html.classList.contains('light')) {
+//     img.setAttribute('src', './assets/myft/Avatar.png')
+// }else {
+//     img.setAttribute('src', './assets/myft/Avatar-light.png')
